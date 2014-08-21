10125a1f5337e7b58d956a162a14590d0ca0de46
liujianglong <757617012@qq.com>
2014-08-21 13:47:36 +0800

    1 增加log4j配置 apache/logging/log4j    http://www.cnblogs.com/backpacker/archive/2012/12/10/2812100.html log4j 2.0与以往的1.x有一个明显的不同，其配置文件只能采用.xml, .json或者 .jsn 八个级别 OFF、FATAL、ERROR、WARN、INFO、DEBUG、TRACE、 ALL 配置文件中写哪个，就会输出哪个级别以上的信息

--- first_hibernate/src/com/liujl/hibernate/Client.java
+++ first_hibernate/src/com/liujl/hibernate/Client.java
@@ -2,15 +2,20 @@ package com.liujl.hibernate;
 
 import java.util.Date;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+
 public class Client {
+	private static final Logger logger = (Logger) LogManager.getLogger(Client.class.getName());
 	//在main方法中完成所有代码
 	public static void main(String[] args) {
 		//读取hibernate.cfg.xml文件
 		Configuration config=new Configuration().configure();
+		
 		/**
 		 * 创建SessionFactory,
 		 * 一个数据库对应一个SessionFactory
@@ -26,7 +31,6 @@ public class Client {
 		 * session用完后,必须关闭
 		 * session是非线程安全的，一般是一个请求对应一个session
 		 */
-		
 		Session session=null;
 		try{
 			session=factory.openSession();
@@ -44,20 +48,23 @@ public class Client {
 			 * 我们不需要写那么多jdbc代码，只需要利用session保存对象，至于hibernate怎么存储对象这里我们不需要关心它
 			 * 这些都由hibernate完成，我们只需要创建对象后交给hibernate就行了
 			 */
+			logger.entry();//这句话是什么意思
+			logger.info("将要保存user");
 			session.save(u);
 			//提交事务
 			session.getTransaction().commit();
-
+			logger.info("保存成功");
 		}catch (Exception e) {
 			//保存数据发生异常，回滚
 			session.getTransaction().rollback();
+			logger.error("保存失败，事务回滚");
 			e.printStackTrace();
 		}finally{
 			//关闭连接
 			if(session!=null){
 				session.close();
+				logger.info("session连接关闭");
 			}
 		}
-		
 	}
 }
